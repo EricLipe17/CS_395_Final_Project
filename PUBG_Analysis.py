8477a1eb7b83a3d71b28240a6e1594a4ec591faa
Eric <EricLipe17@gmail.com>
2019-04-09 14:38:58 -0600

    Quick Commit.

--- PUBG_Analysis.py
+++ PUBG_Analysis.py
@@ -1,16 +1,15 @@
 # This file will contain the neural network that analyzes the PUBG Dataset.
-
-import os
+from time import gmtime, strftime
+# import os
 import tensorflow as tf
 import pandas as pd
-import numpy as np
 
 # Turning warning and info messages off:
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 # Importing Data:
-print("Importing Data:")
+print("Importing Data: " + str(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
 pd.set_option('display.max_columns', None)
 train_data = pd.read_csv("D:\\PUBG_preprocessed_training_data.csv")
 validation_data = pd.read_csv("D:\\PUBG_preprocessed_validation_data.csv")
@@ -24,13 +23,13 @@ train_IDS = pd.DataFrame(train_data["Id"])
 train_targets = pd.DataFrame(train_data["winPlacePerc"])
 train_inputs = train_data.drop(["Unnamed: 0", "Id", "winPlacePerc"], axis=1)
 
-print("\nBegin Training: ")
+print("\nBegin Training: " + str(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
 
 
 # Setting Constants:
 input_size = 66
 output_size = 1
-hidden_layer_size = 150
+hidden_layer_size = 50#150
 
 # Building the Model:
 tf.reset_default_graph()
@@ -50,17 +49,17 @@ weights_3 = tf.get_variable("Weights_3", [hidden_layer_size, hidden_layer_size])
 biases_3 = tf.get_variable("Biases_3", [hidden_layer_size])
 outputs_3 = tf.nn.relu(tf.matmul(outputs_2, weights_3) + biases_3)
 
-weights_4 = tf.get_variable("Weights_4", [hidden_layer_size, hidden_layer_size])
-biases_4 = tf.get_variable("Biases_4", [hidden_layer_size])
-outputs_4 = tf.nn.relu(tf.matmul(outputs_3, weights_4) + biases_4)
-
-weights_5 = tf.get_variable("Weights_5", [hidden_layer_size, hidden_layer_size])
-biases_5 = tf.get_variable("Biases_5", [hidden_layer_size])
-outputs_5 = tf.nn.relu(tf.matmul(outputs_4, weights_5) + biases_5)
+# weights_4 = tf.get_variable("Weights_4", [hidden_layer_size, hidden_layer_size])
+# biases_4 = tf.get_variable("Biases_4", [hidden_layer_size])
+# outputs_4 = tf.nn.relu(tf.matmul(outputs_3, weights_4) + biases_4)
+#
+# weights_5 = tf.get_variable("Weights_5", [hidden_layer_size, hidden_layer_size])
+# biases_5 = tf.get_variable("Biases_5", [hidden_layer_size])
+# outputs_5 = tf.nn.relu(tf.matmul(outputs_4, weights_5) + biases_5)
 
 weights_final = tf.get_variable("Weights_Final", [hidden_layer_size, output_size])
 biases_final = tf.get_variable("Biases_Final", [output_size])
-output = tf.nn.relu(tf.matmul(outputs_5, weights_final) + biases_final)
+output = tf.nn.relu(tf.matmul(outputs_3, weights_final) + biases_final)
 
 mean_loss = tf.losses.mean_squared_error(labels=targets, predictions=output)
 abs_mean_loss = tf.reduce_mean(tf.losses.absolute_difference(labels=targets, predictions=output))
@@ -75,7 +74,6 @@ initializer = tf.global_variables_initializer()
 prev_validation_loss = 9999999
 
 sess.run(initializer)
-print("Running Algorithm: ")
 epoch = 1
 while True:
     _, square_loss, absolute_loss = sess.run([optimize, mean_loss, abs_mean_loss],
@@ -98,6 +96,6 @@ while True:
     prev_validation_loss = validation_absolute_loss
     epoch += 1
 
-print("End of Training.")
+print("End of Training: " + str(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
 sess.close()
 
