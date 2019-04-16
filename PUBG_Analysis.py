537b665fe7a2731338c0143f26ec044ee45bb642
Eric <EricLipe17@gmail.com>
2019-04-15 18:07:08 -0600

    This commit will be the last one that contains updates for the 'PUBG_Analysis.ipynb' file. Jupyter seems to be causing issues when running the model, thus we will be switching to Pycharm and the 'PUBG_Analysis.py' file to continue optimizing the model. Also, this commit contains some word documents that were required by the instructor describing the problem we are trying to solve. Lastly, and most importantly, this commit also contains the 'PUBG_Batching_Class.py' file. This file is a Class Kevin and I wrote to handle batch training for our model.

--- PUBG_Analysis.py
+++ PUBG_Analysis.py
@@ -1,66 +1,121 @@
-# This file will contain the neural network that analyzes the PUBG Dataset.
-from time import gmtime, strftime
 import tensorflow as tf
 import pandas as pd
+import numpy as np
+from time import localtime, strftime
+from PUBG_Batching_Class import PUBG_Data_Reader
+
+batch_size = 50
+
+train_data = PUBG_Data_Reader('PUBG_preprocessed_training_data.csv', batch_size)
+validation_data = PUBG_Data_Reader('PUBG_preprocessed_validation_data.csv')
 
 
-# Importing Data:
-print("Importing Data: ")
-pd.set_option('display.max_columns', None)
-train_data = pd.read_csv("D:\\PUBG_preprocessed_training_data.csv")
-validation_data = pd.read_csv("D:\\PUBG_preprocessed_validation_data.csv")
-
-print("\nMapping Validation Data:")
-validation_targets = pd.DataFrame(validation_data['winPlacePerc'])
-validation_inputs = validation_data.drop(["Unnamed: 0", "Id", "winPlacePerc"], axis=1)
-
-print("\nMapping Train Data")
-train_IDS = pd.DataFrame(train_data["Id"])
-train_targets = pd.DataFrame(train_data["winPlacePerc"])
-train_inputs = train_data.drop(["Unnamed: 0", "Id", "winPlacePerc"], axis=1)
-
-print("\nBegin Training: " + str(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
-
-
-# Setting Constants:
+# Setting Constants
 input_size = 66
 output_size = 1
 hidden_layer_size = 50
+# Update, 5 layers still seems to be optimal with 50 nodes per layer. Now that we added batch training we got the loss
+# to be 0.0662424117. Again, Relu is still the best.
 
-# Building the Model:
+# Building the Model
 tf.reset_default_graph()
 
 inputs = tf.placeholder(tf.float32, [None, input_size])
 targets = tf.placeholder(tf.float32, [None, output_size])
+# learning_rate = tf.placeholder(tf.float32, [])
 
 weights_1 = tf.get_variable("Weights_1", [input_size, hidden_layer_size])
 biases_1 = tf.get_variable("Biases_1", [hidden_layer_size])
-outputs_1 = tf.nn.relu(tf.matmul(inputs, weights_1) + biases_1)
+outputs_1 = tf.nn.sigmoid(tf.matmul(inputs, weights_1) + biases_1)
 
 weights_2 = tf.get_variable("Weights_2", [hidden_layer_size, hidden_layer_size])
 biases_2 = tf.get_variable("Biases_2", [hidden_layer_size])
-outputs_2 = tf.nn.relu(tf.matmul(outputs_1, weights_2) + biases_2)
+outputs_2 = tf.nn.sigmoid(tf.matmul(outputs_1, weights_2) + biases_2)
 
 weights_3 = tf.get_variable("Weights_3", [hidden_layer_size, hidden_layer_size])
 biases_3 = tf.get_variable("Biases_3", [hidden_layer_size])
-outputs_3 = tf.nn.relu(tf.matmul(outputs_2, weights_3) + biases_3)
+outputs_3 = tf.nn.sigmoid(tf.matmul(outputs_2, weights_3) + biases_3)
 
 weights_4 = tf.get_variable("Weights_4", [hidden_layer_size, hidden_layer_size])
 biases_4 = tf.get_variable("Biases_4", [hidden_layer_size])
-outputs_4 = tf.nn.relu(tf.matmul(outputs_3, weights_4) + biases_4)
+outputs_4 = tf.nn.sigmoid(tf.matmul(outputs_3, weights_4) + biases_4)
 
 weights_5 = tf.get_variable("Weights_5", [hidden_layer_size, hidden_layer_size])
 biases_5 = tf.get_variable("Biases_5", [hidden_layer_size])
-outputs_5 = tf.nn.relu(tf.matmul(outputs_4, weights_5) + biases_5)
+outputs_5 = tf.nn.sigmoid(tf.matmul(outputs_4, weights_5) + biases_5)
+
+# weights_6 = tf.get_variable("Weights_6", [hidden_layer_size, hidden_layer_size])
+# biases_6 = tf.get_variable("Biases_6", [hidden_layer_size])
+# outputs_6 = tf.nn.relu(tf.matmul(outputs_5, weights_6) + biases_6)
+#
+# weights_7 = tf.get_variable("Weights_7", [hidden_layer_size, hidden_layer_size])
+# biases_7 = tf.get_variable("Biases_7", [hidden_layer_size])
+# outputs_7 = tf.nn.relu(tf.matmul(outputs_6, weights_7) + biases_7)
+#
+# weights_8 = tf.get_variable("Weights_8", [hidden_layer_size, hidden_layer_size])
+# biases_8 = tf.get_variable("Biases_8", [hidden_layer_size])
+# outputs_8 = tf.nn.relu(tf.matmul(outputs_7, weights_8) + biases_8)
+#
+# weights_9 = tf.get_variable("Weights_9", [hidden_layer_size, hidden_layer_size])
+# biases_9 = tf.get_variable("Biases_9", [hidden_layer_size])
+# outputs_9 = tf.nn.relu(tf.matmul(outputs_8, weights_9) + biases_9)
+#
+# weights_10 = tf.get_variable("Weights_10", [hidden_layer_size, hidden_layer_size])
+# biases_10 = tf.get_variable("Biases_10", [hidden_layer_size])
+# outputs_10 = tf.nn.relu(tf.matmul(outputs_9, weights_10) + biases_10)
+
+# weights_11 = tf.get_variable("Weights_11", [hidden_layer_size, hidden_layer_size])
+# biases_11 = tf.get_variable("Biases_11", [hidden_layer_size])
+# outputs_11 = tf.nn.relu(tf.matmul(outputs_10, weights_11) + biases_11)
+
+# weights_12 = tf.get_variable("Weights_12", [hidden_layer_size, hidden_layer_size])
+# biases_12 = tf.get_variable("Biases_12", [hidden_layer_size])
+# outputs_12 = tf.nn.relu(tf.matmul(outputs_11, weights_12) + biases_12)
+
+# weights_13 = tf.get_variable("Weights_13", [hidden_layer_size, hidden_layer_size])
+# biases_13 = tf.get_variable("Biases_13", [hidden_layer_size])
+# outputs_13 = tf.nn.relu(tf.matmul(outputs_12, weights_13) + biases_13)
+
+# weights_14 = tf.get_variable("Weights_14", [hidden_layer_size, hidden_layer_size])
+# biases_14 = tf.get_variable("Biases_14", [hidden_layer_size])
+# outputs_14 = tf.nn.relu(tf.matmul(outputs_13, weights_14) + biases_14)
+
+# weights_15 = tf.get_variable("Weights_15", [hidden_layer_size, hidden_layer_size])
+# biases_15 = tf.get_variable("Biases_15", [hidden_layer_size])
+# outputs_15 = tf.nn.relu(tf.matmul(outputs_14, weights_15) + biases_15)
+
+# weights_16 = tf.get_variable("Weights_16", [hidden_layer_size, hidden_layer_size])
+# biases_16 = tf.get_variable("Biases_16", [hidden_layer_size])
+# outputs_16 = tf.nn.relu(tf.matmul(outputs_15, weights_16) + biases_16)
+
+# weights_17 = tf.get_variable("Weights_17", [hidden_layer_size, hidden_layer_size])
+# biases_17 = tf.get_variable("Biases_17", [hidden_layer_size])
+# outputs_17 = tf.nn.relu(tf.matmul(outputs_16, weights_17) + biases_17)
+
+# weights_18 = tf.get_variable("Weights_18", [hidden_layer_size, hidden_layer_size])
+# biases_18 = tf.get_variable("Biases_18", [hidden_layer_size])
+# outputs_18 = tf.nn.relu(tf.matmul(outputs_17, weights_18) + biases_18)
+
+# weights_19 = tf.get_variable("Weights_19", [hidden_layer_size, hidden_layer_size])
+# biases_19 = tf.get_variable("Biases_19", [hidden_layer_size])
+# outputs_19 = tf.nn.relu(tf.matmul(outputs_18, weights_19) + biases_19)
+
+# weights_20 = tf.get_variable("Weights_20", [hidden_layer_size, hidden_layer_size])
+# biases_20 = tf.get_variable("Biases_20", [hidden_layer_size])
+# outputs_20 = tf.nn.relu(tf.matmul(outputs_19, weights_20) + biases_20)
 
 weights_final = tf.get_variable("Weights_Final", [hidden_layer_size, output_size])
 biases_final = tf.get_variable("Biases_Final", [output_size])
-output = tf.nn.relu(tf.matmul(outputs_3, weights_final) + biases_final)
+output = tf.nn.sigmoid(tf.matmul(outputs_5, weights_final) + biases_final)
 
-# mean_loss = tf.losses.mean_squared_error(labels=targets, predictions=output)
+mean_loss = tf.losses.mean_squared_error(labels=targets, predictions=output)
 abs_mean_loss = tf.reduce_mean(tf.losses.absolute_difference(labels=targets, predictions=output))
 
-optimize = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(abs_mean_loss)
+optimize = tf.train.AdamOptimizer(learning_rate=0.001).minimize(mean_loss)
+
+
+# Keeping track of the time pre-training:
+print("\n\nStart Time: " + str(strftime("%Y-%m-%d %H:%M:%S", localtime())))
 
 # Initializing the session for the Model to run:
 sess = tf.InteractiveSession()
@@ -69,19 +124,42 @@ initializer = tf.global_variables_initializer()
 
 prev_validation_loss = 9999999
 
+# lr_schedule = [0.001, 0.00001, 0.00000001]
+
 sess.run(initializer)
+print("\n\nRunning Algorithm: ")
 epoch = 1
 while True:
-    _, absolute_loss = sess.run([optimize, abs_mean_loss], feed_dict={inputs: train_inputs, targets: train_targets})
+#     _, absolute_loss, squared_loss = sess.run([optimize, abs_mean_loss, mean_loss], feed_dict={inputs: train_inputs, targets: train_targets})
 
-    validation_absolute_loss = sess.run(abs_mean_loss, feed_dict={inputs: validation_inputs,
-                                                                  targets: validation_targets})
+#     validation_absolute_loss = sess.run(abs_mean_loss, feed_dict={inputs: validation_inputs, targets: validation_targets})
 
-    print('Epoch ' + str(epoch) +
-          #'. Training Square Loss: ' + '{0:.5f}'.format(square_loss) +
-          '. Training Abs Loss: ' + '{0:.5f}'.format(absolute_loss) +
-          #'. Validation Square Loss: ' + '{0:.5f}'.format(validation_square_loss) +
-          '. Validation Abs Loss: ' + '{0:.5f}'.format(validation_absolute_loss))
+    curr_square_epoch_loss = 0.
+    curr_abs_epoch_loss = 0.
+
+    for input_batch, target_batch in train_data:
+        _, batch_loss, batch_abs_mean_loss = sess.run([optimize, mean_loss, abs_mean_loss],
+                                                      feed_dict={inputs: input_batch, targets: target_batch})
+
+        curr_square_epoch_loss += batch_loss
+        curr_abs_epoch_loss += batch_abs_mean_loss
+
+    curr_square_epoch_loss /= train_data.batch_count
+    curr_abs_epoch_loss /= train_data.batch_count
+
+    validation_square_loss = 0.
+    validation_absolute_loss = 0.
+
+    for input_batch, target_batch in validation_data:
+        validation_square_loss, validation_absolute_loss = sess.run([mean_loss, abs_mean_loss],
+                                                                    feed_dict={inputs: input_batch,
+                                                                               targets: target_batch})
+
+    print('Epoch ' +str(epoch) +
+         '. Training Loss: ' + '{0:.10f}'.format(curr_square_epoch_loss) +
+         '. Training Abs Loss: ' + '{0:.10f}' .format(curr_abs_epoch_loss) +
+         '. Validation Loss: ' + '{0:.10f}'.format(validation_square_loss) +
+         '. Validation Abs Loss: ' + '{0:.10f}'.format(validation_absolute_loss))
 
     if validation_absolute_loss > prev_validation_loss:
         print("Early Stopping Activated.\n")
@@ -90,6 +168,9 @@ while True:
     prev_validation_loss = validation_absolute_loss
     epoch += 1
 
-print("End of Training: " + str(strftime("%Y-%m-%d %H:%M:%S", gmtime())))
+print("End of Training.")
+print("End Time: " + str(strftime("%Y-%m-%d %H:%M:%S", localtime())))
 sess.close()
+
+
 
---
+++ PUBG_Batching_Class.py
@@ -0,0 +1,60 @@
+import pandas as pd
+
+
+# Create a class that will do the batching for the algorithm
+# This code is extremely reusable. You should just change Audiobooks_data everywhere in the code
+class PUBG_Data_Reader():
+    pd.set_option('display.max_columns', None)
+
+    # Dataset is a mandatory arugment, while the batch_size is optional
+    # If you don't input batch_size, it will automatically take the value: None
+    def __init__(self, dataset, batch_size=None):
+    
+        # The dataset that loads is one of "train", "validation", "test".
+        # e.g. if I call this class with x('train',5), it will load 'Audiobooks_data_train.npz' with a batch size of 5.
+        train = pd.read_csv(dataset)
+        
+        # Two variables that take the values of the inputs and the targets. Inputs are floats, targets are integers
+        self.inputs, self.targets = pd.DataFrame(train.iloc[:, 2:68]), pd.DataFrame(train.iloc[:, -1])
+        self.inputs_shape = self.inputs.shape
+        self.targets_shape = self.targets.shape
+        
+        # Counts the batch number, given the size you feed it later
+        # If the batch size is None, we are either validating or testing, so we want to take the data in a single batch
+        if batch_size is None:
+            self.batch_size = self.inputs.shape[0]
+        else:
+            self.batch_size = batch_size
+        self.curr_batch = 0
+        self.batch_count = self.inputs.shape[0] // self.batch_size
+    
+    def shape_of(self):
+        return self.inputs_shape, self.targets_shape
+
+    def inputs_head(self):
+        return self.inputs.head()
+    
+    # A method which loads the next batch
+    def __next__(self):
+        if self.curr_batch >= self.batch_count:
+            self.curr_batch = 0
+            raise StopIteration()
+            
+        # You slice the dataset in batches and then the "next" function loads them one after the other
+        batch_slice = slice(self.curr_batch * self.batch_size, (self.curr_batch + 1) * self.batch_size)
+        inputs_batch = self.inputs[batch_slice]
+        targets_batch = self.targets[batch_slice]
+        self.curr_batch += 1
+        
+        # The function will return the inputs batch and the one-hot encoded targets
+        return inputs_batch, targets_batch
+    
+
+    # A method needed for iterating over the batches, as we will put them in a loop
+    # This tells Python that the class we're defining is iterable, i.e. that we can use it like:
+    # for input, output in data: 
+        # do things
+    # An iterator in Python is a class with a method __next__ that defines exactly how to iterate through its objects
+    def __iter__(self):
+        return self
+
