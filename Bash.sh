378c6bf3449c4b49a27df86bad8e2e7e07002b12
Eric <EricLipe17@gmail.com>
2019-04-15 18:13:38 -0600

    This commit will be the last one that contains updates for the 'PUBG_Analysis.ipynb' file. Jupyter seems to be causing issues when running the model, thus we will be switching to Pycharm and the 'PUBG_Analysis.py' file to continue optimizing the model. Also, this commit contains some word documents that were required by the instructor describing the problem we are trying to solve. Lastly, and most importantly, this commit also contains the 'PUBG_Batching_Class.py' file. This file is a Class Kevin and I wrote to handle batch training for our model.

--- Bash.sh
+++ Bash.sh
@@ -3,4 +3,5 @@
 #git push --all --set-upstream origin
 #git commit C:\Users\retic\Desktop\CS_395_Final_Project\Bash.sh
 
+# This command resets the last push as long as we are pointing to head.
 #git reset --soft HEAD~1
